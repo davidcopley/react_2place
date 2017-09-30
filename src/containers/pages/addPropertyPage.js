9e0e665b692ff40acedfa031039feede2702dbf8
David <david.copley@monash.edu>
2017-10-01 03:53:33 +1100

    remove console log

--- src/containers/pages/addPropertyPage.js
+++ src/containers/pages/addPropertyPage.js
@@ -107,7 +107,6 @@ class AddPropertyPage extends React.Component {
                 const newPropertyId = parseInt(res.headers.location.split('/').slice(-1)[0])
                 postPropertyImages(newPropertyId,this.state.images)
             }).catch(err=>{
-                console.log(err)
                 this.setState({errors:err.response.data.errors})
             })
 
